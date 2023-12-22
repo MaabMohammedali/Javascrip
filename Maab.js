5ce8a3a09a9d94dd0699194fa28610fd4c96b0dc
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2023-12-22 19:13:47 +0100

    Add quiz question that use radio buttons

--- Maab.js
+++ Maab.js
@@ -1,7 +1,7 @@
 function formValidate() {
      // this function worked once click on submit button
     document.getElementById('error-message').innerHTML = '';// innerHTML property  used to get  the HTML content within an element
-      // valid name
+      // VALID NAME
     // I use this link to solve this part :https://www.codexworld.com/how-to/validate-first-last-name-with-regular-expression-using-javascript/
     var firstName = document.getElementById('firstName').value; //  method to access the input field by its ID
     var lastName = document.getElementById('lastName').value;
@@ -12,9 +12,10 @@ function formValidate() {
         return;
     }
 
-    // Validate email
+    // VALID EMAIL
     var email = document.getElementById('email').value;
     var emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // to check if the email has a valid format
+
     if (!emailRegex.test(email)) {
         document.getElementById('error-message').innerHTML = 'Please enter a valid email address';
         return;
