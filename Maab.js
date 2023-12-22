d7b1c642d3730fd21253c478bb758668b9d77356
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2023-12-22 22:14:21 +0100

    Show error messege texts for the visitor in case of any validation errors

--- Maab.js
+++ Maab.js
@@ -1,6 +1,6 @@
 function formValidate() {
      // this function worked once click on submit button
-    document.getElementById('error-message').innerHTML = '';// innerHTML property  used to get  the HTML content within an element
+    document.getElementById('error-message').textContent = '';// innerHTML property  used to get  the HTML content within an element
       // VALID NAME
     // I use this link to solve this part :https://www.codexworld.com/how-to/validate-first-last-name-with-regular-expression-using-javascript/
     var firstName = document.getElementById('firstName').value; //  method to access the input field by its ID
@@ -11,6 +11,10 @@ function formValidate() {
         document.getElementById('error-message').textContent  = 'Try again only letter are accepted';
         return;
     }
+    /* if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+        displayErrorMessage('Try again, only letters are accepted');
+        return;
+    }*/
 
     // VALID EMAIL
     var email = document.getElementById('email').value;
@@ -20,6 +24,11 @@ function formValidate() {
         document.getElementById('error-message').textContent  = 'Please enter a valid email address';
         return;
     }
+    /*if (!emailRegex.test(email)) {
+        displayErrorMessage('Please enter a valid email address');
+        return;
+    }*/
+
     // Validate required questions are not left unanswered
     var requiredQuestions = ['q1', 'q2', 'q8', 'q10'];
     var isValid = true;
@@ -36,6 +45,15 @@ function formValidate() {
     if (!isValid) {
         document.getElementById('error-message').textContent = 'Required questions must be answered';
         return;
+    }
+
+    /*if (!isValid) {
+        displayErrorMessage('Please answer all required questions');
+        return;
+    }*/
+
+    function displayErrorMessage(message) {
+        document.getElementById('error-message').textContent = message;
     }
 
 }
